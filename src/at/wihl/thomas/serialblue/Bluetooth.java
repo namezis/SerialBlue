1776488b5b056f8854976b4b8e5ba847bfa430f0
Thomas Wihl <thomas@speedi>
2013-10-04 17:55:52 +0200

    bluetooth connection working

---
+++ src/at/wihl/thomas/serialblue/Bluetooth.java
@@ -0,0 +1,96 @@
+package at.wihl.thomas.serialblue;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
+import java.util.UUID;
+
+import android.app.Activity;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothSocket;
+import android.content.Intent;
+
+public class Bluetooth {
+	final UUID SERIAL_UUID = UUID.fromString("00001101-0000-1000-8000-00805F9B34FB"); //UUID for serial connection
+	final int REQUEST_ENABLE_BT = 4711;
+	BluetoothAdapter mAdapter = null;
+	BluetoothSocket mSocket = null;
+	OutputStream mWrite = null;
+	InputStream mRead = null;
+	boolean mEnabled = false;
+	Thread mReaderThread = null;
+	
+	Boolean init(Activity activity) {
+		mAdapter = BluetoothAdapter.getDefaultAdapter();
+		if (mAdapter == null) {
+		    // Device does not support Bluetooth
+		    return false;
+		}
+		if (!mAdapter.isEnabled()) {
+			//make sure the device's bluetooth is enabled
+		    Intent enableBluetooth = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+		    activity.startActivityForResult(enableBluetooth, REQUEST_ENABLE_BT);
+		}
+		return true;
+	}
+	
+	void onActivityResult(int requestCode, int resultCode, Intent data) {
+		if (requestCode == REQUEST_ENABLE_BT)
+		{
+			if (resultCode == Activity.RESULT_OK) mEnabled = true;
+			else mEnabled = false;
+		}
+		
+	}
+	
+	List<String> scan() {
+		Set<BluetoothDevice> devices = mAdapter.getBondedDevices();
+		List<String> list = new ArrayList<String>();
+		for (BluetoothDevice d : devices) {
+			list.add(d.getName() + " (" + d.getAddress() + ")");
+		}
+		return list;
+	}
+	
+	Runnable readerTask = new Runnable() {
+        @Override
+        public void run() {
+            try {
+                System.out.println("Reading...");
+                byte[] buffer = new byte[64];
+                while (true) {
+                	int bytesRead = mRead.read(buffer);
+                	System.out.println(new String(buffer, 0, bytesRead));
+                }
+            } catch (IOException e) {
+                System.err.println("Connection closed.");
+            }
+        }
+    };
+	
+	Boolean connect(String deviceName) {
+		String[] parts = deviceName.split("\\(|\\)");
+		BluetoothDevice device = mAdapter.getRemoteDevice(parts[1]);
+		try {
+		    mSocket = device.createRfcommSocketToServiceRecord(SERIAL_UUID); 
+		} catch (IOException e) {
+			return false;
+		}
+
+		try {           
+		    mSocket.connect(); 
+		    mWrite = mSocket.getOutputStream();
+		    mRead = mSocket.getInputStream();
+		    mReaderThread = new Thread(readerTask);
+		    mReaderThread.start();
+		    mWrite.write("Hello World\n".getBytes());
+		} catch (IOException e) {
+			return false;
+		}
+		return true;
+	}
+}
--- src/at/wihl/thomas/serialblue/MainActivity.java
+++ src/at/wihl/thomas/serialblue/MainActivity.java
@@ -1,11 +1,20 @@
 package at.wihl.thomas.serialblue;
 
+import java.util.List;
+
 import android.os.Bundle;
 import android.app.Activity;
+import android.content.Intent;
 import android.graphics.Bitmap;
 import android.graphics.Color;
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
 import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.ImageView;
+import android.widget.TextView;
 
 
 /* sdptool records 00:06:66:4D:62:AF
@@ -28,117 +37,167 @@ Language Base Attr List:
   gives the one above.
 */
 
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements SensorEventListener {
 
+	final private int GROUP_DEVICES = 2;
 	private Bitmap mColorWheel;
+	private TextView mTextAccel;
 	private ImageView mColorWheelView;
-	
+	private SensorManager mSensorManager;
+	private Sensor mAccelerometer;
+	private Bluetooth mBluetooth = new Bluetooth();
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
+        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
         setContentView(R.layout.activity_main);
-        int width = 6 * 255;
-        int height = 2 * 255;
-        int[] color = new int[width * height];
-        for (int y = 0; y < height; ++y)
-        {
-        	int x = 0;
-        	for (int x1 = 0; x1 < 255; ++x1)
-        	{
-            	int r = 255 - y, g = 255 - y, b = 255 - y;
-            	r += 255;
-        		g += x1;
-        		if (r < 0) r = 0;
-        		if (r > 255) r = 255;
-        		if (g < 0) g = 0;
-        		if (g > 255) g = 255;
-        		if (b < 0) b = 0;
-        		if (b > 255) b = 255;
-        		color[x + y * width] = Color.rgb(r, g, b);
-        		++x;
-        	}
-        	for (int x1 = 0; x1 < 255; ++x1)
-        	{
-            	int r = 255 - y, g = 255 - y, b = 255 - y;
-        		r += 255 - x1;
-        		g += 255;
-        		if (r < 0) r = 0;
-        		if (r > 255) r = 255;
-        		if (g < 0) g = 0;
-        		if (g > 255) g = 255;
-        		if (b < 0) b = 0;
-        		if (b > 255) b = 255;
-        		color[x + y * width] = Color.rgb(r, g, b);
-        		++x;
-        	}
-        	for (int x1 = 0; x1 < 255; ++x1)
-        	{
-            	int r = 255 - y, g = 255 - y, b = 255 - y;
-        		g += 255;
-        		b += x1;
-        		if (r < 0) r = 0;
-        		if (r > 255) r = 255;
-        		if (g < 0) g = 0;
-        		if (g > 255) g = 255;
-        		if (b < 0) b = 0;
-        		if (b > 255) b = 255;
-        		color[x + y * width] = Color.rgb(r, g, b);
-        		++x;
-        	}
-        	for (int x1 = 0; x1 < 255; ++x1)
-        	{
-            	int r = 255 - y, g = 255 - y, b = 255 - y;
-        		g += 255 - x1;
-        		b += 255;
-        		if (r < 0) r = 0;
-        		if (r > 255) r = 255;
-        		if (g < 0) g = 0;
-        		if (g > 255) g = 255;
-        		if (b < 0) b = 0;
-        		if (b > 255) b = 255;
-        		color[x + y * width] = Color.rgb(r, g, b);
-        		++x;
-        	}
-        	for (int x1 = 0; x1 < 255; ++x1)
-        	{
-            	int r = 255 - y, g = 255 - y, b = 255 - y;
-        		b += 255;
-        		r += x1;
-        		if (r < 0) r = 0;
-        		if (r > 255) r = 255;
-        		if (g < 0) g = 0;
-        		if (g > 255) g = 255;
-        		if (b < 0) b = 0;
-        		if (b > 255) b = 255;
-        		color[x + y * width] = Color.rgb(r, g, b);
-        		++x;
-        	}
-        	for (int x1 = 0; x1 < 255; ++x1)
-        	{
-            	int r = 255 - y, g = 255 - y, b = 255 - y;
-        		b += 255 - x1;
-        		r += 255;
-        		if (r < 0) r = 0;
-        		if (r > 255) r = 255;
-        		if (g < 0) g = 0;
-        		if (g > 255) g = 255;
-        		if (b < 0) b = 0;
-        		if (b > 255) b = 255;
-        		color[x + y * width] = Color.rgb(r, g, b);
-        		++x;
-        	}
-        }
-        mColorWheel = Bitmap.createBitmap(color, width, height, Bitmap.Config.ARGB_8888);
-        mColorWheelView = (ImageView)findViewById(R.id.colorWheel);
-        mColorWheelView.setImageBitmap(mColorWheel);
+//        int width = 6 * 255;
+//        int height = 2 * 255;
+//        int[] color = new int[width * height];
+//        for (int y = 0; y < height; ++y)
+//        {
+//        	int x = 0;
+//        	for (int x1 = 0; x1 < 255; ++x1)
+//        	{
+//            	int r = 255 - y, g = 255 - y, b = 255 - y;
+//            	r += 255;
+//        		g += x1;
+//        		if (r < 0) r = 0;
+//        		if (r > 255) r = 255;
+//        		if (g < 0) g = 0;
+//        		if (g > 255) g = 255;
+//        		if (b < 0) b = 0;
+//        		if (b > 255) b = 255;
+//        		color[x + y * width] = Color.rgb(r, g, b);
+//        		++x;
+//        	}
+//        	for (int x1 = 0; x1 < 255; ++x1)
+//        	{
+//            	int r = 255 - y, g = 255 - y, b = 255 - y;
+//        		r += 255 - x1;
+//        		g += 255;
+//        		if (r < 0) r = 0;
+//        		if (r > 255) r = 255;
+//        		if (g < 0) g = 0;
+//        		if (g > 255) g = 255;
+//        		if (b < 0) b = 0;
+//        		if (b > 255) b = 255;
+//        		color[x + y * width] = Color.rgb(r, g, b);
+//        		++x;
+//        	}
+//        	for (int x1 = 0; x1 < 255; ++x1)
+//        	{
+//            	int r = 255 - y, g = 255 - y, b = 255 - y;
+//        		g += 255;
+//        		b += x1;
+//        		if (r < 0) r = 0;
+//        		if (r > 255) r = 255;
+//        		if (g < 0) g = 0;
+//        		if (g > 255) g = 255;
+//        		if (b < 0) b = 0;
+//        		if (b > 255) b = 255;
+//        		color[x + y * width] = Color.rgb(r, g, b);
+//        		++x;
+//        	}
+//        	for (int x1 = 0; x1 < 255; ++x1)
+//        	{
+//            	int r = 255 - y, g = 255 - y, b = 255 - y;
+//        		g += 255 - x1;
+//        		b += 255;
+//        		if (r < 0) r = 0;
+//        		if (r > 255) r = 255;
+//        		if (g < 0) g = 0;
+//        		if (g > 255) g = 255;
+//        		if (b < 0) b = 0;
+//        		if (b > 255) b = 255;
+//        		color[x + y * width] = Color.rgb(r, g, b);
+//        		++x;
+//        	}
+//        	for (int x1 = 0; x1 < 255; ++x1)
+//        	{
+//            	int r = 255 - y, g = 255 - y, b = 255 - y;
+//        		b += 255;
+//        		r += x1;
+//        		if (r < 0) r = 0;
+//        		if (r > 255) r = 255;
+//        		if (g < 0) g = 0;
+//        		if (g > 255) g = 255;
+//        		if (b < 0) b = 0;
+//        		if (b > 255) b = 255;
+//        		color[x + y * width] = Color.rgb(r, g, b);
+//        		++x;
+//        	}
+//        	for (int x1 = 0; x1 < 255; ++x1)
+//        	{
+//            	int r = 255 - y, g = 255 - y, b = 255 - y;
+//        		b += 255 - x1;
+//        		r += 255;
+//        		if (r < 0) r = 0;
+//        		if (r > 255) r = 255;
+//        		if (g < 0) g = 0;
+//        		if (g > 255) g = 255;
+//        		if (b < 0) b = 0;
+//        		if (b > 255) b = 255;
+//        		color[x + y * width] = Color.rgb(r, g, b);
+//        		++x;
+//        	}
+//        }
+//        mColorWheel = Bitmap.createBitmap(color, width, height, Bitmap.Config.ARGB_8888);
+//        mColorWheelView = (ImageView)findViewById(R.id.colorWheel);
+//        mTextAccel = (TextView)findViewById(R.id.textAccel);
+//        mColorWheelView.setImageBitmap(mColorWheel);
+        mBluetooth.init(this);
     }
 
+    protected void onResume() {
+        super.onResume();
+        mSensorManager.registerListener(this, mAccelerometer, SensorManager.SENSOR_DELAY_NORMAL);
+    }
 
+    protected void onPause() {
+        super.onPause();
+        mSensorManager.unregisterListener(this);
+    }
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
+    @Override
+    public boolean onPrepareOptionsMenu (Menu menu) {
+    	menu.removeGroup(GROUP_DEVICES);
+    	List<String> devices = mBluetooth.scan();
+    	for (String s : devices) {
+    		menu.add(GROUP_DEVICES, Menu.NONE, Menu.NONE, s);
+    	}
+    	return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+    	if (item.getGroupId() == GROUP_DEVICES) {
+    		mBluetooth.connect(item.getTitle().toString());
+    	}
+    	else {
+            switch (item.getItemId()) {
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    	}
+    	return true;
+    }
     
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+    }
+
+    public void onSensorChanged(SensorEvent event) {
+    	if (mTextAccel != null) mTextAccel.setText("Angle is " + String.format("%.2f", Math.atan(event.values[1] / event.values[0])));
+    }
+    
+    protected void onActivityResult (int requestCode, int resultCode, Intent data) {
+    	mBluetooth.onActivityResult(requestCode, resultCode, data);
+    }
 }
